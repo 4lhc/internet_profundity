d064ff18b98045d87dc4dafe66c2f4f17fd62a9b
444lhc <444lhc@gmail.com>
2020-12-14 22:46:20 +0530

    Add docstring

--- profound.py
+++ profound.py
@@ -10,39 +10,79 @@ import random
 from helpers import randomstring, censor
 
 
-
 def fetch_html(url):
-    """accepts url and fetches content"""
+    """
+    Returns html content
+
+        Parameters:
+            url (str): URL string
+
+        Returns:
+            html (bytes): If successfully got response
+            None: If failed to get response
+    """
+
     user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US; rv:1.9.0.13) Gecko/2009073022 Firefox/3.0.13 (.NET CLR 3.5.30729)'
-    referer = 'http://www.google.com'
+    referer = 'http://www.duckduckgo.com'
     headers = { 'User-Agent' : user_agent, 'Referer' : referer }
     req = urllib.request.Request(url, None, headers)
     try:
         response = urllib.request.urlopen(req)
         return response.read()
     except urllib.error.HTTPError as er:
-        print("error code :{} ".format(er.code))
+        print("Error Code :{} ".format(er.code))
         return None
-    
+
 def get_sentence(textdata):
-    #regex match all sentences
+    """
+    Perform regex match to match and return a random sentence. Sentences with word count between min_len and max_len are returned.
+
+        Parameters:
+            textdata (str): Text on which to perform regex match
+
+        Returns:
+            sentence (str):
+    """
+    min_len = 8
+    max_len = 20
+
     pattern = re.compile(r'([A-Z][a-z, "]*[\.!?])')
     sentences = re.findall(pattern, textdata)
-    #get only sentences that are >= 8 words and <=20
-    slist = [s for s in sentences if len(s.split()) >= 8 and len(s.split()) <= 20 ] 
-    #return random item
+
+    slist = [s for s in sentences
+            if len(s.split()) >= min_len
+            and len(s.split()) <= max_len]
     return random.choice(slist)
 
 
 def ddg(kw):
-    """perform a duckduckgo search of a random string"""
+    """
+    perform a duckduckgo search of a random string
+
+        Parameters:
+            kw (str): Search keyword
+        Return:
+            html(str): utf-8 decoded html
+    """
     ddg_url="https://duckduckgo.com/?q={}+f:htm".format(kw)
     return fetch_html(ddg_url).decode('utf-8')
-    
 
-def internet_profundity():
-    html = ddg(randomstring(3))
-    soup = BeautifulSoup(html,  "html.parser", parse_only=SoupStrainer('a', {'class': 'result__a'}, href=True))
+
+def internet_profundity(source=True):
+    """
+    Scrap ddg search with a random string.
+
+        Parameters:
+            source (bool): If True, return the source url too.
+
+        Returns:
+            profound (str): Returns a sentence
+
+    """
+    kw_len = 3
+    html = ddg(randomstring(kw_len))
+    soup_strainer = SoupStrainer('a', {'class': 'result__a'}, href=True)
+    soup = BeautifulSoup(html,  "html.parser", parse_only=soup_strainer)
 
     links = []
     for link in soup:
@@ -57,15 +97,18 @@ def internet_profundity():
 
     html_next = fetch_html(url_next)
     soup = BeautifulSoup(html_next, "html.parser")
-    jk = censor(get_sentence(soup.get_text()))
-    print(f'"{jk}"\n\t\t~ {url_next}')
+    absurdity = censor(get_sentence(soup.get_text()))
+    if source:
+        absurdity = f'"{absurdity}"\n\t\t~ {url_next}'
 
+    return f'"{absurdity}"'
 
 
 if __name__ == "__main__":
     while True:
         try:
-            internet_profundity()
+            print(internet_profundity(source=False))
             break
         except:
-            print(random.choice(['The internet refuses to speak... ¯\_(ツ)_/¯', 'The internet is silent...']))
+            print(random.choice(['The internet refuses to speak... ¯\_(ツ)_/¯',
+                                 'The internet is silent...']))
