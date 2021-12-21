f2bd63a7f7c2db2bea4a55f7076ed6b32980458f
444lhc <444lhc@gmail.com>
2021-12-21 23:26:48 +0530

    App

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python profound.py
--- profound.py
+++ profound.py
@@ -24,7 +24,7 @@ def fetch_html(url):
 
     user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US; rv:1.9.0.13) Gecko/2009073022 Firefox/3.0.13 (.NET CLR 3.5.30729)'
     referer = 'http://www.duckduckgo.com'
-    headers = { 'User-Agent' : user_agent, 'Referer' : referer }
+    headers = {'User-Agent' : user_agent, 'Referer' : referer}
     req = urllib.request.Request(url, None, headers)
     try:
         response = urllib.request.urlopen(req)
@@ -32,6 +32,7 @@ def fetch_html(url):
     except urllib.error.HTTPError as er:
         print("Error Code :{} ".format(er.code))
         return None
+
 
 def get_sentence(textdata):
     """
@@ -50,8 +51,8 @@ def get_sentence(textdata):
     sentences = re.findall(pattern, textdata)
 
     slist = [s for s in sentences
-            if len(s.split()) >= min_len
-            and len(s.split()) <= max_len]
+             if len(s.split()) >= min_len
+             and len(s.split()) <= max_len]
     return random.choice(slist)
 
 
@@ -64,7 +65,7 @@ def ddg(kw):
         Return:
             html(str): utf-8 decoded html
     """
-    ddg_url="https://duckduckgo.com/?q={}+f:htm".format(kw)
+    ddg_url = "https://duckduckgo.com/?q={}+f:htm".format(kw)
     return fetch_html(ddg_url).decode('utf-8')
 
 
@@ -91,10 +92,8 @@ def internet_profundity(source=True):
         p2 = urllib.parse.parse_qs(p1.query)
         links.append(p2['uddg'][0])
 
-    #select a random url from links
+    # select a random url from links
     url_next = random.choice(links)
-
-
     html_next = fetch_html(url_next)
     soup = BeautifulSoup(html_next, "html.parser")
     absurdity = censor(get_sentence(soup.get_text()))
@@ -111,4 +110,5 @@ if __name__ == "__main__":
             break
         except:
             print(random.choice(['The internet refuses to speak... ¯\_(ツ)_/¯',
-                                 'The internet is silent...']))
+                                 'The internet is silent...',
+                                 'Blame my myopic maker...']))
